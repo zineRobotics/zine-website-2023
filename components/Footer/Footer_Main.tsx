8e7db9919bf2b2ffa587c6c81228bd3a01345690
VinVash <2020kucp1046@iiitkota.ac.in>
2021-10-16 10:17:51 +0530

    Navbar and Footer

--- components/Footer/Footer_Main.tsx
+++ components/Footer/Footer_Main.tsx
@@ -1,42 +1,26 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import logo from '../../images/zinelogo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer_Main = () => {
     return(
-        <div className="bg-footer-bg bg-cover bg-center flex justify-around items-start pt-16 mt-96">
+        <div className="bg-footer-bg bg-cover bg-center flex justify-evenly items-start pb-8 pt-16 mt-96">
             <div>
                 <div className="w-32 h-32 mb-16"><Image src={logo}></Image></div>
-                <h1 className="text-4xl text-blue-900 font-light mb-4">Contact Us</h1>
-                <FontAwesomeIcon icon={faGithub} className="text-blue-900 mr-4" size="3x" />
-                <FontAwesomeIcon icon={faLinkedin} className="text-blue-900 mr-4" size="3x" />
-                <FontAwesomeIcon icon={faInstagram} className="text-blue-900 mr-4" size="3x" />
+                <h1 className="text-4xl text-blue-900 font-light mb-4">Connect With <br /> Zine</h1><br />
+                <div className="flex cursor-pointer">
+                    <Link href = "https://github.com/zine-robotics"><FontAwesomeIcon icon={faGithub} className="text-blue-900 mr-4" size="3x" /></Link>
+                    <Link href = "https://www.linkedin.com/company/de-zine-limited/"><FontAwesomeIcon icon={faLinkedin} className="text-blue-900 mr-4" size="3x" /></Link>
+                    <Link href = "https://instagram.com/zine.robotics?utm_medium=copy_link"><FontAwesomeIcon icon={faInstagram} className="text-blue-900 mr-4" size="3x" /></Link>
+                </div>
             </div>
             <div>
-                <h1 className="text-3xl text-blue-900 font-light mb-6">Lorem Ipsum</h1>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-32">Lorem</p>
-            </div>
-            <div>
-                <h1 className="text-3xl text-blue-900 font-light mb-6">Lorem Ipsum</h1>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-1">Lorem</p>
-                <p className="text-xl text-blue-900 font-medium mb-32">Lorem</p>
-            </div>
-            <div>
-                <h1 className="text-3xl text-blue-900 font-light mb-6">Lorem Ipsum</h1>
-                <p className="text-xl text-blue-900 font-medium mb-3">The best robotics club in the country.</p>
-                <button className="bg-white hover:bg-black hover:text-white text-xl text-black py-3 px-12 rounded-full mb-32">Apply Now</button>
+                <h1 className="text-3xl text-blue-900 font-light mb-6">Want to join?</h1>
+                <p className="text-xl text-blue-900 font-medium mb-3 pb-8 ">The best robotics club in the country.</p>
+                <button className="bg-white hover:bg-black hover:text-white text-xl text-blue-700 py-3 px-12 rounded-full mb-32">Apply Now</button>
             </div>
         </div>
     )
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,6 +3,11 @@ import hamburger from "../../images/hamburger.svg";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import home from "../../images/home.png";
+import team from "../../images/team.svg";
+import project from "../../images/project.png"
+import achievement from "../../images/achievement.png"
+import about from "../../images/about.png"
+import blog from "../../images/blog.png"
 
 const Navbar = () => {
   const [Hide, setHide] = useState(false);
@@ -26,28 +31,39 @@ const Navbar = () => {
   interface nav_detail {
     text: string;
     image: StaticImageData;
+    href: string;
   }
 
   const Navbardata: nav_detail[] = [
     {
       text: "Home",
       image: home,
+      href: "/",
+    },
+    {
+      text: "Team",
+      image: team,
+      href: "/team",
+    },
+    {
+      text: "Projects",
+      image: project,
+      href: "/projects",
+    },
+    {
+      text: "Achievements",
+      image: achievement,
+      href: "/achievements",
     },
     {
       text: "About",
-      image: home,
+      image: about,
+      href: "/about",
     },
     {
-      text: "Projects & Events",
-      image: home,
-    },
-    {
-      text: "Core Members",
-      image: home,
-    },
-    {
-      text: "Contact Us",
-      image: home,
+      text: "Blogs",
+      image: blog,
+      href: "/blogs",
     },
   ];
 
@@ -56,23 +72,21 @@ const Navbar = () => {
       <>
         <div className="z-30 top-0 flex fixed w-full h-full animate-navbar">
           <div
-            className="z-20 bg-cover bg-gray-300 opacity-50 w-full h-full transparent"
+            className="z-20 bg-cover bg-gray-500 opacity-50 w-full h-full transparent"
             onClick={() => {
               setHide(false);
             }}
           ></div>
-          <div className="top-0 right-0 w-auto bg-blue-900 fixed h-full overflow-auto z-30">
+          <div className="top-0 right-0 w-auto bg-white fixed h-full overflow-auto z-30">
             <div className="pt-6 pb-12 px-20">
-              <button className="bg-yellow-500 hover:bg-yellow-500 rounded py-2 px-6 text-3xl font-bold">
-                <h1>Welcome to Zine</h1>
-              </button>
+            <h1 className="text-3xl text-blue-700 font-bold">Welcome to Zine</h1>
             </div>
             <div>
               {Navbardata.map((item, index) => (
-                <div key={index}>
-                  <h1 className="text-white text-3xl px-20 py-10">
+                <div key={index} className="bg-yellow-500 rounded-xl w-88 mx-4 my-4">
+                  <h1 className="text-blue-800 text-3xl px-20 py-6">
                     <Image src={item.image} width="24" height="24"></Image>
-                    <a href="#"> {item.text}</a>
+                    <a href={item.href}> {item.text}</a>
                   </h1>
                 </div>
               ))}
@@ -81,7 +95,7 @@ const Navbar = () => {
               <div className="rounded-full overflow-hidden">
                 <Image src={logo} width="64" height="64"></Image>
               </div>
-              <button className="bg-yellow-500 hover:bg-green-500 rounded py-2 px-3 text-3xl font-bold">
+              <button className="bg-blue-300 hover:bg-green-500 rounded py-2 px-3 text-3xl font-bold">
                 <h1> More About Zine</h1>
               </button>
             </div>
