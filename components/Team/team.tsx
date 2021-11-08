febfebda7543350de354947b9e16e2ccbd8a83e7
VinVash <2020kucp1046@iiitkota.ac.in>
2021-11-08 16:45:59 +0530

    tinkering about the team page.

--- components/Team/team.tsx
+++ components/Team/team.tsx
@@ -3,28 +3,28 @@ import Link from "next/link";
 
 const Team = () => {
     return(
-        <div className="bg-white min-h-screen flex items-center justify-center px-16">
-  <div className="fixed w-full max-w-lg">
-    <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute top-20 left-80 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute bottom-20 left-140 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute bottom-20 right-120 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute -bottom-120 left-150 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute bottom-40 left-160 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute bottom-40 right-160 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute -bottom-120 right-160 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute -bottom-40 right-140 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute bottom-20 left-80 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute bottom-60 left-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute -bottom-120 right-120 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute -bottom-40 left-130 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-  </div>
-  <div className="relative grid grid-cols-3 gap m-8 gap-8 text-2xl font-nunito text-black">
-      <button className="transition duration-500 ease-in-out bg-purple-200 px-20 py-8 hover:bg-red-600 hover:text-white transform hover:-translate-y-1 hover:scale-110 ...">
-      <Link href=""><h1>This is the team page.</h1></Link>
-      </button>
-    </div>
-</div>
+      // first div mein h-screen ki problem hai. Change it everwhere.
+      <div className="perspective w-screen bg-black">
+        <div className="font-righteous text-center text-white text-8xl py-8">OUR TEAM</div>
+        <div className="mx-40 grid grid-cols-4 gap-16 h-64 my-8 mb-32 text-white">
+          <div className="col-span-1 rounded-full bg-blue-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-purple-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-green-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-yellow-500">Hello there</div>
+        </div>
+        <div className="mx-40 grid grid-cols-4 gap-16 h-64 my-8 mb-32 text-white">
+          <div className="col-span-1 rounded-full bg-blue-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-purple-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-green-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-yellow-500">Hello there</div>
+        </div>
+        <div className="mx-40 grid grid-cols-4 gap-16 h-64 my-8 mb-32 text-white">
+          <div className="col-span-1 rounded-full bg-blue-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-purple-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-green-500">Hello there</div>
+          <div className="col-span-1 rounded-full bg-yellow-500">Hello there</div>
+        </div>
+      </div>
     )
 }
 
