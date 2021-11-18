596b556c8257253c8e6cee20658d22ba37c53319
VinVash <2020kucp1046@iiitkota.ac.in>
2021-11-18 12:25:10 +0530

    changed navbar

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 import logo from '../../images/zinelogo.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import githublogo from '../../images/github.png';
+import linkedinlogo from '../../images/linkedin.png';
+import instagramlogo from '../../images/instagramicon.jpeg';
 
 const Footer_Main = () => {
     return(
-        <div className="sticky custom:bg-purple-800 z-30 custom:flex custom:flex-wrap gap-4 justify-evenly items-start pb-2 pt-10 mt-6">
-            <div className="box-border bg-indigo-800 custom:p-10 rounded-lg">
-                <div className="w-20 h-20 sm:w-28 sm:h-28 mb-10 pt-5 transform-gpu mx-96 sm:mx-44 md:mx-28"><Image src={logo}></Image></div>
-                <div className="text-4xl transform-gpu mx-96 sm:mx-44 md:mx-28 font-bold font-sans text-gray-1500 mb-6">Connect With ZINE!</div>
-                <div className="flex mx-96 sm:mx-44 md:mx-28 pb-5">
-                    <Link href = "https://github.com/zine-robotics"><FontAwesomeIcon icon={faGithub} className="cursor-pointer text-gray-1000 mr-4" size="3x" /></Link>
-                    <Link href = "https://www.linkedin.com/company/de-zine-limited/"><FontAwesomeIcon icon={faLinkedin} className="cursor-pointer text-gray-1000 mr-6" size="3x" /></Link>
-                    <Link href = "https://instagram.com/zine.robotics?utm_medium=copy_link"><FontAwesomeIcon icon={faInstagram} className="cursor-pointer text-gray-1000 mr-6" size="3x" /></Link>
-                </div>
-            </div>
-            
-            <div className="box-border bg-purple-800 p-5 rounded-lg">
-                <h1 className="text-2xl sm:text-4xl text-gray-1500 font-bold mb-6 text-center ">Register for <br/> Workshop by ZINE <br/> </h1>
-                <p className="text-xl sm:text-2xl text-gray-1000 font-medium mb-3 pb-8 mx-96 sm:mx-0 sm:text-center">Where imagination leads to creation!</p>
-                <div className="text-center">
-                    <button className="bg-white hover:bg-black hover:text-white text-xl text-gray-1000 py-3 px-12 rounded-full">Apply Now</button>
+        <div className="sticky custom:bg-black z-30 custom:flex custom:flex-wrap gap-4 justify-center items-center pb-2 pt-10 mt-6">
+            <Script type="text/javascript" src="/script.js" />
+            <div className="box-border border-2 border-top custom:p-10 rounded-lg">
+                <div className="w-20 h-20 sm:w-28 sm:h-28 mb-10 pt-5 transform-gpu mx-96"><Image src={logo}></Image></div>
+                <div className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500 text-3xl lg:text-3xl xl:text-4xl my-12 text-center">Connect With ZINE!</div>
+                <div className="flex pb-5 gap-8 justify-center">
+                    <div className="w-20 h-20 sm:w-28 sm:h-28 mb-10 pt-5 transform-gpu">
+                    <Link href = "https://github.com/zine-robotics"><Image className="cursor-pointer w-20 h-20" src={githublogo}/></Link>
+                    </div>
+                    <div id="linkedin" className="w-20 h-20 sm:w-28 sm:h-28 mb-10 pt-5 transform-gpu">
+                    <Link href = "https://github.com/zine-robotics"><Image className="cursor-pointer w-20 h-20" src={linkedinlogo}/></Link>
+                    </div>
+                    <div className="w-20 h-20 sm:w-28 sm:h-28 mb-10 pt-5 transform-gpu pt-8">
+                    <Link href = "https://github.com/zine-robotics"><Image className="cursor-pointer w-20 h-20" src={instagramlogo}/></Link>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ components/Footer/script.js
@@ -0,0 +1,2 @@
+var a = document.getElementById("linkedin");
+a.style.backgroundColor="red";
