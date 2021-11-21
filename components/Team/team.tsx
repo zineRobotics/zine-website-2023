2e5e53e3e9ad1d4beff5d8462c8cd4be12b56292
VinVash <2020kucp1046@iiitkota.ac.in>
2021-11-21 15:48:05 +0530

    made the team page responsive

--- components/Team/team.tsx
+++ components/Team/team.tsx
@@ -189,33 +189,33 @@ const secondyear: pic_detail[] = [
 const Team = () => {
     return(
       // first div mein h-screen ki problem hai. Change it everywhere.
-      <div className="text-black bg-white w-screen justify-center px-12 lg:px-24 xl:px-48">
+      <div className="text-black bg-white w-screen justify-center px-12 lg:px-24">
         <h1 className="pt-8 text-4xl text-center">Our Founders: The Roots Of Our Tree</h1>
         <div className="pt-16 grid grid-cols-1 lg:grid-cols-2">
           <div className="col-span-1 w-48 lg:w-80 justify-self-center"><Image className="rounded-full" src={RajeshSir} /></div>
-          <div className="col-span-1">
+          <div className="col-span-1 my-auto">
             <h1 className="text-2xl text-center">DR. RAJESH KUMAR</h1>
             <p className="text-center">Ph.D., PDF(NUS, Singapore) SMIEEE, FIETE, MIE (I),SMIACSIT, LMISTE, MIAENG,</p>
             <p className="text-center">Professor of Elec. Engg.</p>
           </div>
         </div>
-        <div className="grid grid-cols-4 pt-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 pt-16">
         {founders.map((item, index) => (
-                <div key={index} className="w-64">
-                  <h1 className="text-black text-xl py-2 font-nunito">
-                    <div className=""><Image className="rounded-full" src={item.image}></Image></div>
+                <div key={index} className="col-span-1">
+                  <h1 className="text-black text-xl py-2 font-nunito text-center">
+                    <div className="w-48 mx-auto"><Image className="rounded-full" src={item.image} /></div>
                     <p className="text-center">{item.name}</p>
                     <p className="text-center">{item.desc}</p>
                   </h1>
                 </div>
               ))}
         </div>
-        {/* <h1 className="pt-32 text-center font-bold text-4xl">FINAL YEAR</h1>
-        <div className="grid grid-cols-4 pt-16">
+        <h1 className="pt-32 text-center font-bold text-4xl">FINAL YEAR</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 pt-16">
         {finalyear.map((item, index) => (
-          <div key={index} className="w-64">
-            <h1 className="text-black text-xl py-2 font-nunito">
-              <div className=""><Image className="rounded-full" src={item.image}></Image></div>
+          <div key={index} className="col-span-1">
+            <h1 className="text-black text-xl py-2 font-nunito text-center">
+              <div className="w-48 mx-auto"><Image className="rounded-full" src={item.image}></Image></div>
               <p className="text-center">{item.name}</p>
               <p className="text-center">{item.desc}</p>
             </h1>
@@ -223,11 +223,11 @@ const Team = () => {
         ))}
         </div>
         <h1 className="pt-32 text-center font-bold text-4xl">THIRD YEAR</h1>
-        <div className="grid grid-cols-4 pt-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 pt-16">
         {thirdyear.map((item, index) => (
-          <div key={index} className="w-64">
-            <h1 className="text-black text-xl py-2 font-nunito">
-              <div className=""><Image className="rounded-full" src={item.image}></Image></div>
+          <div key={index} className="col-span-1">
+            <h1 className="text-black text-xl py-2 font-nunito text-center">
+              <div className="w-48 mx-auto"><Image className="rounded-full" src={item.image}></Image></div>
               <p className="text-center">{item.name}</p>
               <p className="text-center">{item.desc}</p>
             </h1>
@@ -235,17 +235,17 @@ const Team = () => {
         ))}
         </div>
         <h1 className="pt-32 text-center font-bold text-4xl">SECOND YEAR</h1>
-        <div className="grid grid-cols-4 pt-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 pt-16">
         {secondyear.map((item, index) => (
-          <div key={index} className="w-64">
-            <h1 className="text-black text-xl py-2 font-nunito">
-              <div className=""><Image className="rounded-full" src={item.image}></Image></div>
+          <div key={index} className="col-span-1">
+            <h1 className="text-black text-xl py-2 font-nunito text-center">
+              <div className="w-48 mx-auto"><Image className="rounded-full" src={item.image}></Image></div>
               <p className="text-center">{item.name}</p>
               <p className="text-center">{item.desc}</p>
             </h1>
           </div>
         ))}
-        </div> */}
+        </div>
       </div>
     )
 }
