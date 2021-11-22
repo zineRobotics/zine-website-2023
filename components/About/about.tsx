c11bab93d5f8b7a8953b24692bdf968c38db0ac8
VinVash <2020kucp1046@iiitkota.ac.in>
2021-11-22 10:22:39 +0530

    small change

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import RajeshSir from "../../images/rajeshsir.jpg"
 
 const About = () => {
     return(
-        <div className="bg-white min-h-screen flex items-center justify-center px-16">
-  <div className="fixed w-full max-w-lg">
-    <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute top-20 left-80 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute bottom-20 left-140 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute bottom-20 right-120 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute -bottom-120 left-150 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute bottom-40 left-160 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute bottom-40 right-160 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute -bottom-120 right-160 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute -bottom-40 right-140 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute bottom-20 left-80 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute bottom-60 left-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute -bottom-120 right-120 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="absolute -bottom-40 left-130 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-  </div>
-  <div className="relative grid grid-cols-3 gap m-8 gap-8 text-2xl font-nunito text-black">
-      <button className="transition duration-500 ease-in-out bg-purple-200 px-20 py-8 hover:bg-red-600 hover:text-white transform hover:-translate-y-1 hover:scale-110 ...">
-      <Link href=""><h1>This is the About page.</h1></Link>
-      </button>
-    </div>
-</div>
+        <div className="text-black bg-white w-screen px-12 md:px-24 lg:px-48">
+            <h1 className="text-2xl pt-8">About ZINE</h1>
+            <p className="pt-8">Zine is a creative group of engineering undergraduates of Malaviya National Institute of Technology, Jaipur who are together to learn, improve and apply their technical skills to help foster the growth of society and India in the field of technology by utilising their engineering skills to work on real time problems. It is comprised of students from various disciplines working under guidance of Dr. Rajesh Kumar from Electrical Engineering department and various alumni working in reputed firms and doing research in Universities in India and abroad. Zine has been the only active robotics and research group of MNIT for the last 11 years, since its foundation.</p>
+
+            <h1 className="text-3xl pt-16">Our Background and Mission</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-3 pt-8">
+              <div>
+                <h1 className="text-2xl">What We Do</h1>
+                <p className="pt-8">We regularly participate in various national and international events related to a multitude of fields, publish research articles in reputed publications like IEEE Journals and provide services to industries like consultacy related to automation through embedded system based solution for Micro, Small, Medium scale enterprises(MSME).</p>
+              </div>
+
+              <div>
+                <h1 className="text-2xl">Why We Do It</h1>
+                <p className="pt-8">We believe in using our knowledge for the betterment of the society and the student community by providing ambitious people a platform to develop their ideas in the fields of Product development, Machine Learning, Software Development and Robotics.</p>
+              </div>
+              <div>
+                <h1 className="text-2xl">Who We Are</h1>
+                <p className="pt-8">Zine is a creative group of engineering undergraduates of Malaviya National Institute of Technology, Jaipur. Presently our team comprises of around 30 undergraduates from Electrical Engineering, Mechanical Engineering, Electronics and Communication Engineering and Computer Engineering.</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-5 pt-8">
+              <div><Image src={RajeshSir}/></div>
+            </div>
+        </div>
     )
 }
 
