358e323ef12cc82b4eadf1b5417e6c4bb47aa6dd
VinVash <2020kucp1046@iiitkota.ac.in>
2021-11-22 11:35:06 +0530

    small changes

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -6,7 +6,7 @@ import RajeshSir from "../../images/rajeshsir.jpg";
 const About = () => {
     return(
         <div className="text-black bg-white w-screen">
-            <h1 className="text-2xl pt-8 px-12 lg:px-24 xl:px-48">About ZINE</h1>
+            <h1 className="text-3xl pt-8 px-12 lg:px-24 xl:px-48">About ZINE</h1>
             <p className="pt-8 px-12 lg:px-24 xl:px-48">Zine is a creative group of engineering undergraduates of Malaviya National Institute of Technology, Jaipur who are together to learn, improve and apply their technical skills to help foster the growth of society and India in the field of technology by utilising their engineering skills to work on real time problems. It is comprised of students from various disciplines working under guidance of Dr. Rajesh Kumar from Electrical Engineering department and various alumni working in reputed firms and doing research in Universities in India and abroad. Zine has been the only active robotics and research group of MNIT for the last 11 years, since its foundation.</p>
 
             <h1 className="text-3xl pt-16 px-12 lg:px-24 xl:px-48">Our Background and Mission</h1>
