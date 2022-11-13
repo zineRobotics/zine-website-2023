dab2e3f43851f2332a5548a50887b5d186e0860c
Mahak5457 <78644718+Mahak5457@users.noreply.github.com>
2022-11-14 04:06:44 +0530

    Update team.jsx

--- components/Team/team.jsx
+++ components/Team/team.jsx
@@ -632,26 +632,26 @@ const finalyear = [
 ];
 
 const thirdyear = [
-  // {
-  //   name: "VINAMRA VASHISHTH",
-  //   image: vinamra,
-  //   desc: "Computer Science Engineering",
-  //   github: "",
-  //   linkedin: "https://www.linkedin.com/in/vinamra-vashishth-721785201/",
-  //   twitter: "",
-  //   instagram: "",
-  //   facebook: ""
-  // },
-  //{
-  //  name: "NISHANT GARG",
-  //   image: nishant,
-  //   desc: "Computer Science Engineering",
-  //   github: "",
-  //   linkedin: "https://www.linkedin.com/in/nishant-garg-968205208/",
-  //   twitter: "",
-  //   instagram: "",
-  //   facebook: ""
-  // },
+  {
+    name: "VINAMRA VASHISHTH",
+    image: vinamra,
+    desc: "Computer Science Engineering",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/vinamra-vashishth-721785201/",
+    twitter: "",
+    instagram: "",
+    facebook: ""
+  },
+  {
+   name: "NISHANT GARG",
+    image: nishant,
+    desc: "Computer Science Engineering",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/nishant-garg-968205208/",
+    twitter: "",
+    instagram: "",
+    facebook: ""
+  },
   {
     name: "DIVYANSH GARG",
     image: divyansh,
@@ -662,16 +662,16 @@ const thirdyear = [
     instagram: "",
     facebook: ""
   },
-  // {
-  //   name: "ABHINAV KUMAR",
-  //   image: abhinav,
-  //   desc: "Electrical Engineering",
-  //   github: "",
-  //   linkedin: "https://www.linkedin.com/in/abhinav-kumar-5b2097205/",
-  //   twitter: "",
-  //   instagram: "",
-  //   facebook: ""
-  // },
+  {
+    name: "ABHINAV KUMAR",
+    image: abhinav,
+    desc: "Electrical Engineering",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/abhinav-kumar-5b2097205/",
+    twitter: "",
+    instagram: "",
+    facebook: ""
+  },
   {
     name: "SUDESHNA SONKAR",
     image: sudeshna,
@@ -772,30 +772,30 @@ const Team = () => {
           </div>
         ))}
         </div>
-        {/* <h1 className="pt-12 text-center font-bold text-4xl px-12 lg:px-24">FINAL YEAR</h1> */}
-         {/* <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 pt-16 px-12 lg:px-24">
+        { <h1 className="pt-12 text-center font-bold text-4xl px-12 lg:px-24">FINAL YEAR</h1> }
+         { <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 pt-16 px-12 lg:px-24">
         {
-        // finalyear.map((item, index) => (
-        //   <div key={index} className="col-span-1">
-        //     <h1 className="text-black py-2 font-nunito text-center">
-        //       <div className="w-48 mx-auto member-hover">
-        //         <Image className="rounded-full" src={item.image} placeholder="blur"></Image>
-        //       </div>
-        //       <p className="text-center text-xl font-bold">{item.name}</p>
-        //       <p className="text-center text-gray-600 text-md">{item.desc}</p>
-        //       <div className="text-center">
-        //       <div className="grid grid-flow-col auto-cols-auto mt-2 w-64 mx-auto">
-        //         {item.github !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.facebook}><FontAwesomeIcon icon={faGithub} className="text-black mr-4" size="2x" /></Link></div> : <></>}
-        //         {item.linkedin !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.linkedin}><FontAwesomeIcon icon={faLinkedin} className="text-blue-600 mr-4" size="2x" /></Link></div> : <></>}
-        //         {item.instagram !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.twitter}><FontAwesomeIcon icon={faInstagram} className="text-red-500 mr-4" size="2x" /></Link></div> : <></>}
-        //         {item.facebook !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.facebook}><FontAwesomeIcon icon={faFacebook} className="text-blue-800 mr-4" size="2x" /></Link></div> : <></>}
-        //       </div>
-        //       </div>
-        //     </h1>
-        //   </div>
-        // ))
+        finalyear.map((item, index) => (
+          <div key={index} className="col-span-1">
+            <h1 className="text-black py-2 font-nunito text-center">
+              <div className="w-48 mx-auto member-hover">
+                <Image className="rounded-full" src={item.image} placeholder="blur"></Image>
+              </div>
+              <p className="text-center text-xl font-bold">{item.name}</p>
+              <p className="text-center text-gray-600 text-md">{item.desc}</p>
+              <div className="text-center">
+              <div className="grid grid-flow-col auto-cols-auto mt-2 w-64 mx-auto">
+                {item.github !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.facebook}><FontAwesomeIcon icon={faGithub} className="text-black mr-4" size="2x" /></Link></div> : <></>}
+                {item.linkedin !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.linkedin}><FontAwesomeIcon icon={faLinkedin} className="text-blue-600 mr-4" size="2x" /></Link></div> : <></>}
+                {item.instagram !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.twitter}><FontAwesomeIcon icon={faInstagram} className="text-red-500 mr-4" size="2x" /></Link></div> : <></>}
+                {item.facebook !== "" ? <div className="cursor-pointer transform-gpu transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"><Link href={item.facebook}><FontAwesomeIcon icon={faFacebook} className="text-blue-800 mr-4" size="2x" /></Link></div> : <></>}
+              </div>
+              </div>
+            </h1>
+          </div>
+        ))
         }
-        </div> */}
+        </div> }
       
         <h1 className="pt-12 text-center font-bold text-4xl px-12 lg:px-24">THIRD YEAR</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 py-16 px-12 lg:px-24">
