6e2983c70dd10b9ebda599edebe0c0b345ad11ef
MashaalSayeed <mashaal2403@gmail.com>
2023-03-24 00:08:34 +0530

    Workshops Updated

--- components/Workshops/registration.tsx
+++ components/Workshops/registration.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import Link from "next/link";
 
 const branches = [
@@ -13,27 +13,52 @@ const branches = [
 ]
 
 const Registration = () => {
+    const [state, setState] = useState({
+        name: "",
+        college: "MNIT Jaipur",
+        email: "",
+        branch: "Computer Science & Engineering",
+        phone: "",
+        gender: "",
+        platform: ""
+    });
+
+    const onChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const {id, value} = e.target;
+        setState({ ...state, [id]: value })
+    }
+
+    const onGenderChange = (e: ChangeEvent) => setState({ ...state, gender: e.target.id})
+    const onPlatformChange = (e: ChangeEvent) => setState({ ...state, platform: e.target.id})
+
+
+    const onSubmit = (e: FormEvent) => {
+        console.log("hello")
+        e.preventDefault()
+        console.log(state)
+    }
+
     return (
         <div className="flex flex-col items-center" style={{background: "linear-gradient(to right, #0C72B0, #95C5E2)", marginBottom: -35}}>
             <div className="text-white font-bold">
                 <h1 className="text-2xl mt-12 lg:text-3xl">WORKSHOP REGISTRATION</h1>
             </div>
             <div className="bg-white rounded-xl p-8 md:p-16 mt-8 mb-16 w-11/12 md:w-1/2" style={{maxWidth: 651}}>
-                <form>
+                <form noValidate>
                     <div>
                         <label className="block text-gray-600">Full Name</label>
-                        <input type="text" id="name" className="block w-full focus:outline-none bottom-border text-lg pt-2" placeholder="xyz abc" required />
+                        <input type="text" id="name" className="block w-full focus:outline-none bottom-border text-lg pt-2" placeholder="xyz abc" value={state.name} onChange={onChange} required />
                     </div>
                     <div className="mt-6">
                         <label className="block text-gray-600">College Name</label>
-                        <select id="name" className="block w-full focus:outline-none bottom-border text-lg pt-2 indent-0">
+                        <select id="college" className="block w-full focus:outline-none bottom-border text-lg pt-2 indent-0" value={state.college} onChange={onChange}>
                             <option>MNIT Jaipur</option>
                             <option>IIIT Kota</option>
                         </select>
                     </div>
                     <div className="mt-6">
                         <label className="block text-gray-600">College Email Address</label>
-                        <input type="email" id="email" className="block w-full focus:outline-none bottom-border text-lg pt-2" placeholder="abc@xyz.com" required />
+                        <input type="email" id="email" className="block w-full focus:outline-none bottom-border text-lg pt-2" placeholder="abc@xyz.com" value={state.email} onChange={onChange} required />
                     </div>
                     <div className="mt-6">
                         <label className="block text-gray-600">Branch</label>
@@ -46,44 +71,34 @@ const Registration = () => {
                         </select>
                     </div>
                     <div className="mt-6">
-                        <label className="block text-gray-600">Year</label>
-                        <select id="name" className="block w-full focus:outline-none bottom-border text-lg pt-2 indent-0">
-                            {
-                                [1, 2, 3, 4, 5].map(year => (
-                                    <option key={year}>Year {year}</option>
-                                ))
-                            }
-                        </select>
-                    </div>
-                    <div className="mt-6">
                         <label className="block text-gray-600">Phone Number</label>
-                        <input type="number" id="phone" className="block w-full focus:outline-none bottom-border text-lg pt-2" placeholder="123456789" required />
+                        <input type="number" id="phone" className="block w-full focus:outline-none bottom-border text-lg pt-2" placeholder="123456789" value={state.phone} onChange={onChange} required />
                     </div>
 
                     <div className="mt-6 flex flex-col">
                         <p>Gender</p>
                         <div>
-                            <input className="mr-6" type="radio" name="gender" id="male" />
+                            <input className="mr-6" type="radio" name="gender" id="male" checked={state.gender === "male"} onChange={onGenderChange} />
                             <label htmlFor="male">Male</label>
                         </div>
                         <div>
-                            <input className="mr-6" type="radio" name="gender" id="female"/>
+                            <input className="mr-6" type="radio" name="gender" id="female" checked={state.gender === "female"} onChange={onGenderChange}/>
                             <label htmlFor="female">Female</label>
                         </div>
                     </div>
                     <div className="mt-6 flex flex-col">
                         <p>Platform</p>
                         <div>
-                            <input className="mr-6" type="radio" name="gender" id="ios" />
-                            <label htmlFor="ios">IOS</label>
+                            <input className="mr-6" type="radio" name="platform" id="android" checked={state.platform === "android"} onChange={onPlatformChange}/>
+                            <label htmlFor="android">Android</label>
                         </div>
                         <div>
-                            <input className="mr-6" type="radio" name="gender" id="android"/>
-                            <label htmlFor="android">Android</label>
+                            <input className="mr-6" type="radio" name="platform" id="ios" checked={state.platform === "ios"} onChange={onPlatformChange} />
+                            <label htmlFor="ios">IOS</label>
                         </div>
                     </div>
 
-                    <button type="submit" className="mt-6 p-4 block w-full rounded-3xl text-white" style={{background: "#0C72B0"}}>Register</button>
+                    <button type="submit" className="mt-6 p-4 block w-full rounded-3xl text-white" onSubmit={onSubmit} style={{background: "#0C72B0"}}>Register</button>
                 </form>
             </div>
         </div>
--- components/Workshops/workshops.tsx
+++ components/Workshops/workshops.tsx
@@ -1,61 +1,100 @@
-import React from "react";
+import React, { useState, KeyboardEvent } from "react";
 import Link from "next/link";
-import { Chrono } from "react-chrono";
 
-const blogs = [
+const data = [
     {
-      //cardTitle: "Basic Mechanical Workshop",
-      timelineContent: (<p className="text-white font-bold">Basic Mechanical Workshop</p>),
-      //title: "1st April Saturday",
-      title: "1st April Saturday"
+      name: "Basic Mechanical Workshop",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,quaerat?",
+      timeDate: "1st April Saturday",
+      type: "",
+      venue: "VLTC - L004"
     },
     {
-      cardTitle: "Basic Electrical Electronics Workshop",
-      title: "2nd April Sunday",
+      name: "Basic Electrical Electronics Workshop",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,quaerat?",
+      timeDate: "2nd April Sunday",
+      type: "",
+      venue: "VLTC - L006"
     },
     {
-      cardTitle: "Basic Mechanical Workshop",
-      title: "1st April Saturday",
+      name: "Basic Mechanical Workshop1",
+      timeDate: "1st April Saturday",
     },
     {
-      cardTitle: "Basic Electrical Electronics Workshop",
-      title: "2nd April Sunday",
+      name: "Basic Electrical Electronics Workshop2",
+      timeDate: "2nd April Sunday",
     },
     {
-      cardTitle: "Basic Mechanical Workshop",
-      title: "1st April Saturday",
+      name: "Basic Mechanical Workshop3",
+      timeDate: "1st April Saturday",
     },
     {
-      cardTitle: "Basic Electrical Electronics Workshop",
-      title: "2nd April Sunday",
+      name: "Basic Electrical Electronics Workshop4",
+      timeDate: "2nd April Sunday",
     },
 ];
 
 const Workshops = () => {
+    const [state, setState] = useState({ selected: 0 })
+    const select = (id: number) => {
+      setState({ selected: id })
+    }
+
+    const handleKeyDown = (e: KeyboardEvent<HTMLElement>) => {
+      const { selected } = state;
+      if (e.key === "ArrowUp" && selected > 0) {
+        setState({ selected: selected - 1 })
+      } else if (e.key === "ArrowDown" && selected < data.length - 1) {
+        setState({ selected: selected + 1 })
+      }
+    }
+
     return (
         <div className="flex flex-col items-center" style={{background: "linear-gradient(to right, #0C72B0, #95C5E2)", marginBottom: -35}}>
           <h1 className="text-white font-bold mt-24 text-6xl">Workshop</h1>
-          <button className="mt-8 p-4 block rounded-3xl bg-white" style={{width: 300}}>Register</button>
-          <div className="my-24" style={{ width: "100%", height: "100%" }}>
-            <Chrono 
-                items={blogs} 
-                hideControls 
-                cardHeight={0}
-                cardWidth={250}
-                borderLessCards
-                allowDynamicUpdate
-                mode="VERTICAL_ALTERNATING"
-                theme={{
-                    cardBgColor: 'transparent',
-                    primary: 'white',
-                    secondary: 'transparent',
-                    cardForeColor: 'white',
-                    titleColor: '#C2FFF4',
-                    titleColorActive: '#C2FFF4'
-                    // 95C5E2
-                }}
-            >
-            </Chrono>
+          
+          <Link href="/workshops/registration">
+            <button className="mt-8 p-4 block rounded-3xl bg-white" style={{width: 300}}>Register</button>
+          </Link>
+          
+          {/* Timeline */}
+          <div className="container my-24">
+            <div className="grid grid-cols-9 mx-auto p-2 outline-none" tabIndex={1} onKeyDown={handleKeyDown}>
+              {
+                data.map((item, key) => {
+                  const card = (
+                    <div className={"p-4 my-4 text-white " + (key % 2 === 0 ? "col-start-2 col-end-5 ml-auto text-right" : "col-start-6 col-end-9")}>
+                        <h3 className="font-semibold text-lg mb-1">{item.name}</h3>
+                        {state.selected === key && <p className="leading-tight">{item.description}</p>}
+                    </div>
+                  )
+
+                  const title = (
+                    <div className={"p-4 my-4 my-auto text-white " + (key % 2 !== 0 ? "col-start-2 col-end-5 ml-auto" : "col-start-6 col-end-9")}>
+                      <h3 className="font-semibold text-lg mb-1" style={{color: "#C2FFF4"}}>{item.timeDate}</h3>
+                      <h3 className="font-semibold text-md mb-1" style={{color: "#C2FFF4"}}>{item.venue}</h3>
+                    </div>
+                  )
+
+                  return (
+                    <div key={item.name} className="flex flex-row-reverse contents" onClick={() => select(key)}>
+                      {key % 2 === 0 ? card : title}
+
+                      <div className="col-start-5 col-end-6 mx-auto relative">
+                        <div className="h-full w-8 flex items-center justify-center">
+                          <div className="h-full w-1 bg-white"></div>
+                        </div>
+                        <div className="w-8 h-8 absolute top-1/2 -mt-3 rounded-full bg-white">
+                          {state.selected === key && <div className="w-4 h-4 mx-auto mt-2 rounded-full" style={{background: "#95C5E2"}}></div>}
+                        </div>
+                      </div>
+
+                      {key % 2 === 0 ? title : card}
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
       )
