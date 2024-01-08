b101f3b460532eb14a8a9729889baf5f7d73cfa4
MashaalSayeed <mashaal2403@gmail.com>
2024-01-09 00:48:40 +0530

    Alumni Donation

--- apis/payments.ts
+++ apis/payments.ts
@@ -4,33 +4,25 @@ import { db } from "../firebase";
 
 interface ICreateOrder {
     amount: string;
-    currency: string;
-    receipt: string;
-    remarks: string;
+    // currency: string;
+    // receipt: string;
+    remark: string;
     email: string;
-    userid: string;
+    currency: string;
+
 }
 
-export interface ICreateOrderResult {
-    orderid: string;
-    paymentid: string;
-    amount: string | number;
-}
+// const PAYMENT_BACKEND_SERVER = "http://zinedocker.onrender.com/api"
+const PAYMENT_BACKEND_SERVER = "http://localhost:8080/api"
 
-const PAYMENT_BACKEND_SERVER = "http://localhost:4000"
-const paymentsCollection = collection(db, "payments")
 export const createOrder = async (order: ICreateOrder) => {
-    return axios.post(`${PAYMENT_BACKEND_SERVER}/create-order`, order) //as Promise<ICreateOrderResult>
+    return axios.post(`${PAYMENT_BACKEND_SERVER}/create-order`, order)
 }
 
 interface IVerifyPayment {
-    paymentid: string;
-    orderid: string;
-}
-
-export interface IVerifyPaymentResult {
-    success: boolean;
-    message: string;
+    paymentId: string;
+    orderId: string;
+    signature: string;
 }
 
 export const verifyPayment = async (payment: IVerifyPayment) => {
@@ -38,16 +30,16 @@ export const verifyPayment = async (payment: IVerifyPayment) => {
 }
 
 export interface IDonation {
+    id: number;
     amount: number;
-    date: Date;
-    userid: string;
+    timestamp: number;
     email: string;
     status: string;
-    orderid: string;
-    paymentid: string;
+    type: string;
     remarks: string;
+    orderId: string;
 }
 
-export const fetchAllDonations = async (userid: string) => {
-    return getDocs(paymentsCollection)
+export const fetchAllDonations = async () => {
+    return axios.get(`${PAYMENT_BACKEND_SERVER}/get-donations`)
 }
--- components/Members/Alumni/donate.tsx
+++ components/Members/Alumni/donate.tsx
@@ -4,9 +4,11 @@ import ProtectedRoute from "./ProtectedRoute";
 import { useForm } from "react-hook-form";
 import useRazorpay from "react-razorpay";
 import { useAuth } from "../../../context/authContext";
-import { ICreateOrderResult, IDonation, IVerifyPaymentResult, createOrder, fetchAllDonations, verifyPayment } from "../../../apis/payments";
+import { IDonation, createOrder, fetchAllDonations, verifyPayment } from "../../../apis/payments";
 import { ToastContainer, toast } from "react-toastify";
 import styles from "../../../constants/styles";
+import ZineLogo from "../../../images/admin/logo.png"
+import Image from "next/image";
 
 const MAX_TRANSACTION_LIMIT = 500000
 interface IDonateForm {
@@ -20,7 +22,7 @@ const Donate = () => {
     const [donations, setDonations] = useState<IDonation[]>([])
     const { register, reset, formState: { errors }, handleSubmit } = useForm<IDonateForm>()
     const currencies = ["INR"]
-    const projectNames: string[] = []
+    // const projectNames: string[] = []
 
     const [Razorpay] = useRazorpay();
     const { authUser } = useAuth()
@@ -28,20 +30,22 @@ const Donate = () => {
     const onSubmit = async (data: IDonateForm) => {
         let { amount, currency, remarks } = data
         amount = Math.floor(amount * 100)
-        createOrder({
+        reset({ amount: 0.0, remarks: "" })
+        toast.promise(createOrder({
             amount: amount.toString(),
-            currency,
-            remarks,
-            receipt: "ZINE Donation",
+            currency: currency || "INR",
+            remark: remarks,
             email: authUser!.email,
-            userid: authUser!.uid
+        }), {
+            pending: "Initiating payment"
         }).then(order => {
-            const data = order.data as ICreateOrderResult
+            const orderData = order.data as IDonation
+            console.log(orderData)
             const rzpay = new Razorpay({
-                key: process.env.REACT_APP_RAZORPAY_ID as string,
+                key: process.env.NEXT_PUBLIC_RAZORPAY_KEY as string,
                 amount: amount.toString(),
                 currency: currency,
-                order_id: data.orderid,
+                order_id: orderData.orderId,
                 name: "ZINE",
                 description: remarks,
                 theme: {
@@ -52,11 +56,10 @@ const Donate = () => {
                     email: authUser!.email,
                 },
                 handler: async (response) => {
-                    console.log(response)
-                    const result = await verifyPayment({ orderid: data.orderid, paymentid: data.paymentid })
-                    const resdata = result.data as IVerifyPaymentResult
-                    if (resdata.success) toast.success(resdata.message)
-                    else toast.error(resdata.message)
+                    const result = await verifyPayment({ orderId: orderData.orderId, paymentId: response.razorpay_payment_id, signature: response.razorpay_signature })
+                    const resdata = result.data
+                    if (resdata) toast.success("Payment Successful! Thank you for your donation!")
+                    else toast.error("Error")
                 },
                 modal: {
                     ondismiss: () => toast.info("Payment Cancelled")
@@ -67,23 +70,23 @@ const Donate = () => {
                 toast.error("Payment Failed")
             })
             rzpay.on("payment.ondismiss", () => {
-                toast.info('Payment Cacelled')
+                toast.info('Payment Cancelled')
             });
 
-            rzpay.open()
+            setDonations([...donations, orderData])
+
+            return rzpay.open()
         }).catch(err => {
             toast.error(err)
         })
     }
 
-    // useEffect(() => {
-    //     fetchAllDonations(authUser!.uid).then(res => {
-    //         setDonations(res.docs.map(d => {
-    //             const data = d.data()
-    //             return { ...data, date: new Date(data.date.seconds * 1000) } as IDonation
-    //         }))
-    //     })
-    // }, [])
+    useEffect(() => {
+        fetchAllDonations().then(res => {
+            const donations = res.data.filter((d: IDonation) => d.email === authUser!.email)
+            setDonations(donations)
+        })
+    }, [])
 
 
     return (
@@ -103,12 +106,22 @@ const Donate = () => {
             <div className="flex flex-col md:grid grid-cols-12 h-screen" style={{ background: "#EFEFEF" }}>
                 <SideNav />
 
-                <div className="px-6 col-span-9 md:px-12 flex flex-col">
+                <div className="px-6 col-span-9 md:px-12 flex flex-col overflow-y-scroll">
                     <h1 className="text-4xl font-bold mt-8" style={{ color: "#AAAAAA" }}>Donate</h1>
 
                     <div className="row-span-5 bg-white rounded-xl py-4 px-6 my-8 w-full shadow-md">
                         <h1 className="text-2xl font-bold" style={styles.textPrimary}>Make A Donation</h1>
                         <div className="grid grid-cols-5 gap-6 mt-4">
+                            <div className="col-span-2">
+                                <label className="block text-gray-600 text-sm">Currency</label>
+                                <select id="currencyName" className="block w-full focus:outline-none bottom-border pt-3" {...register("currency")}>
+                                    {
+                                        currencies.map(curr => (
+                                            <option key={curr}>{curr}</option>
+                                        ))
+                                    }
+                                </select>
+                            </div>
                             <div className="col-span-3">
                                 <label className="block text-gray-600 text-sm">Amount<span className="text-red-500">*</span></label>
                                 <div className="relative">
@@ -117,22 +130,12 @@ const Donate = () => {
                                 </div>
                                 {errors.amount && <p className="text-red-500 text-sm" role="alert">Amount is required</p>}
                             </div>
-                            <div className="col-span-2">
+                            {/* <div className="col-span-2">
                                 <label className="block text-gray-600 text-sm">Project Name</label>
                                 <select id="projectName" className="block w-full focus:outline-none bottom-border pt-2" {...register("projectName")}>
                                     {
                                         projectNames.map(project => (
                                             <option key={project}>{project}</option>
-                                        ))
-                                    }
-                                </select>
-                            </div>
-                            {/* <div className="col-span-2">
-                                <label className="block text-gray-600 text-sm">Currency</label>
-                                <select id="currencyName" className="block w-full focus:outline-none bottom-border pt-2" {...register("currency")}>
-                                    {
-                                        currencies.map(curr => (
-                                            <option key={curr}>{curr}</option>
                                         ))
                                     }
                                 </select>
@@ -143,16 +146,40 @@ const Donate = () => {
                                 {errors.remarks && <p className="text-red-500 text-sm" role="alert">Remarks is required</p>}
                             </div>
                         </div>
-                        <button className="p-3 block w-40 rounded-3xl text-white mt-8 shadow-md hover:opacity-80" style={{ background: "#0C72B0" }} onClick={handleSubmit(onSubmit)}>Donate</button>
+                        <button className="p-3 block w-40 rounded-3xl text-white mt-4 shadow-md hover:opacity-80" style={{ background: "#0C72B0" }} onClick={handleSubmit(onSubmit)}>Donate</button>
                     </div>
+
+                    {/* <div className="bg-white py-4 px-6 mb-8 rounded-xl shadow-md">
+                        <h1 className="text-2xl font-bold" style={styles.textPrimary}>Donations Made</h1>
+                        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
+                            {
+                                donations.sort((a, b) => b.timestamp - a.timestamp).map(d => (
+                                    <div className="bg-white rounded-md shadow-md px-4 py-6 text-center">
+                                        <p className="text-3xl font-bold" style={styles.textPrimary}>₹{d.amount / 100}</p>
+                                        <p className="" style={styles.textGray}>{new Date(d.timestamp).toLocaleDateString('en-CA')}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div> */}
 
                     <div className="bg-white py-4 px-6 mb-8 rounded-xl shadow-md">
                         <h1 className="text-2xl font-bold" style={styles.textPrimary}>Donations Made</h1>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col gap-2 mt-4">
                             {
-                                donations.map(d => (
-                                    <div>
-                                        [{d.date.toLocaleDateString('en-CA')}] {d.amount}₹ donated by {d.email}
+                                !donations.length && <p className="font-bold" style={styles.textGray}>You have not made any donations yet!</p>
+                            }
+                            {
+                                donations.sort((a, b) => b.timestamp - a.timestamp).map(d => (
+                                    <div className="bg-white rounded-md shadow-md p-2 md:p-4 flex justify-between items-center" key={d.id}>
+                                        <div className="flex items-center gap-2 mr-1">
+                                            <Image src={ZineLogo} width={50} height={50} />
+                                            <div>
+                                                <div className="font-bold" style={styles.textPrimary}>{authUser!.name}</div>
+                                                <div className="text-sm -mt-1" style={styles.textGray}>{new Date(d.timestamp).toLocaleDateString('en-CA')}</div>
+                                            </div>
+                                        </div>
+                                        <p className="text-xl md:text-3xl font-bold" style={styles.textPrimary}>₹{d.amount / 100}</p>
                                     </div>
                                 ))
                             }
