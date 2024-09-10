42027d3eb303a522d5ac0ff6890fce6aef07dee0
Herschelle Thomas <herschellethomas10@gmail.com>
2024-09-11 02:35:28 +0530

    delete data form

---
+++ pages/delete-account.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  
+  return (
+    <div className="bg-body-bg">
+      <p>data deletion form</p>
+    </div>
+  );
+};
+
+export default Home;
