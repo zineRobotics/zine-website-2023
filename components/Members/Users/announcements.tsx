2dcf9ed6ec0a16f2907725ac85d32e527b3236dd
Herschelle Thomas <herschellethomas10@gmail.com>
2024-11-15 16:15:09 +0530

    hackathon register modal

--- components/Members/Users/announcements.tsx
+++ components/Members/Users/announcements.tsx
@@ -7,9 +7,10 @@ import Image from "next/image";
 import ZineBlog from "../../../images/admin/zineblog.png";
 import { useAuth } from "../../../context/authContext";
 import { getRoom, fetchRoomsByUser } from "../../../apis/room";
-import {checkHackathonRegistration, registerHackathon} from "../../../apis/users"
+import { checkHackathonRegistration, registerHackathon } from "../../../apis/users"
 import { Registration } from "../../Workshops";
 import { toast } from "react-toastify";
+import Modal from "../modal";
 // import { collection, getCountFromServer, query, where } from "firebase/firestore";
 // import { db } from "../../../firebase";
 
@@ -60,6 +61,7 @@ const Announcements = () => {
   const [announcements, setAnnouncements] = useState<IMessageData[]>([]);
   const { authUser } = useAuth();
   const [isRegistered, setIsRegistered] = useState<boolean>();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const date = new Date();
   const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
@@ -77,26 +79,26 @@ const Announcements = () => {
   }
 
   useEffect(() => {
-  //   registerHackathon().then((res) => {
-  //     console.log("ann", res); 
-  // }) .catch((err) => {
-  //   console.log("ann err", err);
-    
-  // })
+    //   registerHackathon().then((res) => {
+    //     console.log("ann", res); 
+    // }) .catch((err) => {
+    //   console.log("ann err", err);
+
+    // })
     checkHackathonRegistration().then((res) => {
-      console.log("ann", res); 
-      if(res.status === "registered") setIsRegistered(true);
+      console.log("ann", res);
+      if (res.status === "registered") setIsRegistered(true);
       else setIsRegistered(false);
-  }) .catch((err) => {
-    console.log("ann err", err);
-  })
+    }).catch((err) => {
+      console.log("ann err", err);
+    })
   }, []);
 
   const _registerHackathon = async () => {
     try {
       registerHackathon().then((res) => {
-        console.log("ann", res); 
-        if(res===true) setIsRegistered(true);
+        console.log("ann", res);
+        if (res === true) setIsRegistered(true);
         else setIsRegistered(false);
       })
     } catch (error) {
@@ -142,20 +144,22 @@ const Announcements = () => {
               </div>
             ) : (
               // <Link href="/aptitudeForm">
-                <div
-                  className="flex flex-col col-span-9 md:col-span-3 row-span-4 rounded-3xl px-8 py-8 shadow-xl hover:border-blue-400 cursor-pointer shadow-md"
-                  style={{ background: "linear-gradient(135deg, #9B9C9C 0%, #D4D4D4 100%)" }}
-                >
-                  <div className="mt-24">
-                    <h1 className="text-2xl text-white font-extrabold">{authUser!.email?.split("@")[0].toUpperCase()}</h1>
-                    <h3 className="text-lg text-white">{authUser!.name} </h3>
-                    <br />
-                    <p className="text-lg text-white">Not Registered for Tank-Wars</p>
-                  </div>
-                  <button className="mt-4 bg-blue2 text-white py-2 px-4 rounded-xl hover:bg-blue1" onClick={_registerHackathon}>
-                    Register
-                  </button>
+              <div
+                className="flex flex-col col-span-9 md:col-span-3 row-span-4 rounded-3xl px-8 py-8 shadow-xl hover:border-blue-400 cursor-pointer shadow-md"
+                style={{ background: "linear-gradient(135deg, #9B9C9C 0%, #D4D4D4 100%)" }}
+              >
+                <div className="mt-24">
+                  <h1 className="text-2xl text-white font-extrabold">{authUser!.email?.split("@")[0].toUpperCase()}</h1>
+                  <h3 className="text-lg text-white">{authUser!.name} </h3>
+                  <br />
+                  <p className="text-lg text-white">Not Registered for Tank-Wars</p>
                 </div>
+                <button className="mt-4 bg-blue2 text-white py-2 px-4 rounded-xl hover:bg-blue1"
+                  onClick={() => setIsOpen(true)}
+                >
+                  Register
+                </button>
+              </div>
               // </Link>
             )}
           </div>
@@ -192,6 +196,17 @@ const Announcements = () => {
               );
             })}
           </div> */}
+          <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+            <div className="p-4 md:p-5 text-center">
+              <svg className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              <h3 className="mb-5 text-lg font-normal text-gray-500">Do you wish to register for Tank-Wars?</h3>
+              <button type="button" className="text-white bg-blue2 hover:bg-blue1 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2" onClick={() => { _registerHackathon().then(() => { setIsOpen(false) }) }}>
+                Yes
+              </button>
+            </div>
+          </Modal>
         </div>
       </div>
     </ProtectedRoute>
